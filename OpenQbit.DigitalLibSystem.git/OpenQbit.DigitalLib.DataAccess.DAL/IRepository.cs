e26d648313e6ff378a10cdff4cdffbb502e2e0e3
Thisara Deshan <thisaradeshan95@gmail.com>
2017-07-10 10:36:12 +0530

    creating IRepository

---
+++ OpenQbit.DigitalLibSystem.git/OpenQbit.DigitalLib.DataAccess.DAL/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.DigitalLib.DataAccess.DAL
+{
+    public interface IRepository
+    {
+        List<T> GetAll<T>() where T : class;
+        
+       
+        bool Create<T>(T obj) where T : class;
+        bool Update<T>(T obj) where T : class;
+        bool Delete<T>(T obj) where T : class;
+        bool Save();
+    }
+}
