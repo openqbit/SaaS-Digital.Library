62b9b71fa36a88b0e99d9bf8e237773fa180701d
Dhanushka Sandaruwan <dhanushkaa@openqubit.com>
2017-07-18 13:53:02 +0530

    Create Test Client  project

---
+++ OpenQbit.DigitalLibSystem.git/OpenQubit.DigitalLib.TestClient.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQubit.DigitalLib.TestClient.Web.MvcApplication" Language="C#" %>
---
+++ OpenQbit.DigitalLibSystem.git/OpenQubit.DigitalLib.TestClient.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQubit.DigitalLib.TestClient.Web.Startup))]
+namespace OpenQubit.DigitalLib.TestClient.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
