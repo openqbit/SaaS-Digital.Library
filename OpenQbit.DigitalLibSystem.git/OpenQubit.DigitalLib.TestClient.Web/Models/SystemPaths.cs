d7c10089d985b3024868dd0f9a454dc33a5821dd
Dhanushka Sandaruwan <dhanushkaa@openqubit.com>
2017-07-18 14:01:25 +0530

    add model class into test project

---
+++ OpenQbit.DigitalLibSystem.git/OpenQubit.DigitalLib.TestClient.Web/Models/SystemPaths.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQubit.DigitalLib.TestClient.Web.Models
+{
+    public class SystemPaths
+    {
+        public int ID { get; set; }
+        public byte[] Path { get; set; }
+        public virtual ICollection<ResourcePath> ResourcePath { get; set; }
+        public virtual Systems System { get; set; }
+    }
+}
---
+++ OpenQbit.DigitalLibSystem.git/OpenQubit.DigitalLib.TestClient.Web/Models/Systems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQubit.DigitalLib.TestClient.Web.Models
+{
+    public class Systems
+    {
+        public int ID { get; set; }
+        public string SystemName { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string GuardianName { get; set; }
+        public virtual ICollection<SystemPaths> SystemPaths { get; set; }
+    }
+}
